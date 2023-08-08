f829f22ce078224d828a3fd2fe15eb5c4b57b5ba
Aniket Raikar <aniketraikar82@gmail.com>
2023-08-08 16:37:22 +0530

    first commit

---
+++ database/db.js
@@ -0,0 +1,13 @@
+import mongoose from 'mongoose';
+
+const Connection = async (username, password) => {
+    const URL = `mongodb://aniketraikar99:l5qsw8KwG5Nv8vb6@ac-xrhxe6d-shard-00-00.2hlocna.mongodb.net:27017,ac-xrhxe6d-shard-00-01.2hlocna.mongodb.net:27017,ac-xrhxe6d-shard-00-02.2hlocna.mongodb.net:27017/?ssl=true&replicaSet=atlas-9275l7-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    try {
+        await mongoose.connect(URL, { useNewUrlParser: true })
+        console.log('Database connected successfully');
+    } catch (error) {
+        console.log('Error while connecting to the database ', error);
+    }
+};
+
+export default Connection;
